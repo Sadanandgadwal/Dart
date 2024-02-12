46fc59f21c77c8ee5c2bca90047ebf6bbd9cecb0
Sadanand gadwal <83832977+Sadanandgadwal@users.noreply.github.com>
2024-02-12 14:44:05 +0530

    Create first.dart

---
+++ first.dart
@@ -0,0 +1,31 @@
+void main() {
+  var name = "sadanand";
+  print(name);
+
+  const age =23;
+  print(age);
+  
+  const isOpen = true;
+  print(isOpen);
+  
+  print(age+10);
+  print(age-10);
+  print(age*10);
+  print(age/10);
+  print(age%10);
+
+  
+  print("my name is $name and my age is $age");
+  
+  //signal line comment
+  //print("my name is" + " " + name);
+  
+  
+  //multi line comment
+  /* print(age+10);
+  print(age-10);
+  print(age*10);
+  print(age/10);
+  print(age%10);
+  */
+}
